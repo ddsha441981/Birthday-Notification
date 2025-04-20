bbf58221fea2e9d3b37c5de57a70da633f527601
ddsha441981 <ddsa44191@gmail.com>
2025-04-20 18:24:15 +0530

    HAL Explorer added

--- src/main/java/com/cwc/birthday/notification/repository/BirthdayRepository.java
+++ src/main/java/com/cwc/birthday/notification/repository/BirthdayRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
+@RepositoryRestResource(exported = false)
 public interface BirthdayRepository extends JpaRepository<Birthday, Long>{//QuerydslPredicateExecutor<Birthday>
 
     @Query("SELECT b FROM Birthday b WHERE FUNCTION('MONTH', b.birthDate) = :month AND FUNCTION('DAY', b.birthDate) = :day")
--- src/main/java/com/cwc/birthday/notification/repository/ScheduledMessageRepository.java
+++ src/main/java/com/cwc/birthday/notification/repository/ScheduledMessageRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RepositoryRestResource(exported = false)
 public interface ScheduledMessageRepository extends JpaRepository<ScheduledMessage, Long> {// QuerydslPredicateExecutor<ScheduledMessage>
     List<ScheduledMessage> findByScheduledAtBeforeAndStatus(LocalDateTime time, MessageStatus status);
 
